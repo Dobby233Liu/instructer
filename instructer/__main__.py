ef525b11af1551417aea960cca99aa9c0f6e0462
Dobby233Liu <15816141883@163.com>
2021-07-18 10:25:55 +0800

    ok

--- instructer/__main__.py
+++ instructer/__main__.py
@@ -1,18 +1,12 @@
 #!/usr/bin/env python3
 
 import click
-from .__version__ import __version__, __program_name__
 
 @click.command()
 def main():
-	print(__program_name__)
-	print("version %s" % __version__)
-
-	print("") # newline
-
-	print("This is working")
+    print("This is working")
 
 if __name__ == "__main__":
-	main()
+    main()
 else:
-	raise NotImplementedError()
+    raise NotImplementedError()
--- instructer/__version__.py
+++
@@ -1,3 +0,0 @@
-__version__ = "1.0.0"
-__version_info__ = (1, 0, 0)
-__program_name__ = "instructer"
